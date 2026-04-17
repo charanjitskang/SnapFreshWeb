fbd00d32b21ef20cd2a86ed0d3ff3c9f5a63a308
Charanjit Kang <ckang@investcloud.com>
2026-04-17 12:56:15 +0100

    SSR entry fix

--- src/entries/ssr.tsx
+++ src/entries/ssr.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
 import { renderToString } from 'react-dom/server';
 import { DataDeletionPage } from '../pages/DataDeletionPage';
+import { DisclaimerPage } from '../pages/DisclaimerPage';
 import { HomePage } from '../pages/HomePage';
+import { NutritionMethodologyPage } from '../pages/NutritionMethodologyPage';
 import { PrivacyPage } from '../pages/PrivacyPage';
 import { SupportPage } from '../pages/SupportPage';
 import { TermsPage } from '../pages/TermsPage';
@@ -12,7 +14,9 @@ const pageRegistry = {
   '/privacy/': <PrivacyPage />,
   '/terms/': <TermsPage />,
   '/support/': <SupportPage />,
-  '/data-deletion/': <DataDeletionPage />
+  '/data-deletion/': <DataDeletionPage />,
+  '/nutrition-methodology/': <NutritionMethodologyPage />,
+  '/disclaimer/': <DisclaimerPage />
 };
 
 export function renderPage(pathname: keyof typeof pageRegistry) {
